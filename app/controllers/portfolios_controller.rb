0e23907d8a28694c8770c5b9b7ecae5c2770c3eb
Mariori <mariamakantasi@gmail.com>
2019-01-12 17:04:57 +0200

    portfolio update show and delete implementation

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
 	def index
 		@portfolio_items=Portfolio.all
 	end	
@@ -7,15 +8,43 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end	
 
+	def show
+    end
+
+	def edit
+ 	end
+
 	def create
 		@portfolio_item = Portfolio.new(portfolio_params)
-		 respond_to do |format|
-		      if @portfolio_item.save
-		        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
-		        format.json { render :show, status: :created, location: @portfolio_item }
-		      end
+		respond_to do |format|
+	       if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+
+	       end
+		end
+	end	
+
+	def update
+		respond_to do |format|
+		    if @portfolio_item.update(portfolio_params)
+		        format.html { redirect_to @portfolio_item, notice: 'Portfolio Item successfully updated.' }
 		    end
-		end	
+    	end
+	end
+
+	def destroy
+	    @portfolio_item.destroy
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: 'Portfolio Item successfully destroyed.' }
+	    end
+	end
+
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
 	# Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
