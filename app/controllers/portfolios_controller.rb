163b81a791d5e87378606a1db858a9b31a26ffb6
Mariori <mariamakantasi@gmail.com>
2019-01-12 16:36:26 +0200

    intergrate new/create action in portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,5 +3,23 @@ class PortfoliosController < ApplicationController
 		@portfolio_items=Portfolio.all
 	end	
 
+	def new
+		@portfolio_item = Portfolio.new
+	end	
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_params)
+		 respond_to do |format|
+		      if @portfolio_item.save
+		        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+		        format.json { render :show, status: :created, location: @portfolio_item }
+		      end
+		    end
+		end	
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 
 end
