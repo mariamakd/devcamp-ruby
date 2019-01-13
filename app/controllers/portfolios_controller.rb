676ca3b16a137e5ec92fd74bc6f4a42f2bb679da
Mariori <mariamakantasi@gmail.com>
2019-01-13 17:39:15 +0200

    create new technologies within portfolio form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,6 +12,7 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
+		3.times{@portfolio_item.technologies.build}
 	end	
 
 	def show
@@ -54,7 +55,7 @@ class PortfoliosController < ApplicationController
 
 	# Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body,technologies_attributes:[:name])
     end
 
 end
