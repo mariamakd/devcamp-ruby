46518d101b272a7247e0d20c3661d47ec63b610d
Mariori <mariamakantasi@gmail.com>
2019-01-13 20:59:37 +0200

    added navbar and default header

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,11 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+	included do
+		before_action :set_title
+	end
+
+	def set_title
+		@page_title="DevcampPortfolio | My Portfolio Website"	
+		@seo_keywords="Maria Makantasi's portfolio"
+	end
+end
