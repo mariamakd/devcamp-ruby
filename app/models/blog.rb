c178e67e00a04b63fb3b45f8fec0b3c0b2888b99
Mariori <mariamakantasi@gmail.com>
2019-01-13 20:17:07 +0200

    some refactoring

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,14 @@ class Blog < ApplicationRecord
 	extend FriendlyId
     friendly_id :title, use: :slugged
 
+    after_initialize :set_defaults
+
     validates_presence_of :title, :body
 
     belongs_to :topic
+
+	def set_defaults
+		self.topic_id ||= Topic.last.id
+	end
+
 end
