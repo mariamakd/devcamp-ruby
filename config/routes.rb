9c39f3773b28b19bbad36db5164689163652a606
Mariori <mariamakantasi@gmail.com>
2019-01-18 18:33:40 +0200

    Implemented persistent position update for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '',path_names: {sign_in:'login', sign_out: 'logout', sign_up: 'register'}
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -9,8 +11,8 @@ Rails.application.routes.draw do
   resources :blogs do
   	member do
   		get :toggle_status
-  	end	
-  end	
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
