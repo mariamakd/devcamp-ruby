66fcb4648be81e875031e57455728acd712cac1b
Mariori <mariamakantasi@gmail.com>
2019-01-19 20:53:56 +0200

    refactored alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -62,5 +62,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Maria Makantsis Portfolio", sticky: false, time: 1000)
+  end
+
 
 end
