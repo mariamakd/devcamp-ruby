0716299764195ca5a7ce9f013ec2d4011211baf0
Mariori <mariamakantasi@gmail.com>
2019-01-23 20:18:41 +0200

    Twitter implementation added and tweets appear in tech news

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
 
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
+
   resources :blogs do
   	member do
   		get :toggle_status
